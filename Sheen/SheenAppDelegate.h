9317b4152cca2ff6b367863e2d7bb1a0a492270e
Matthew Ewer <Ewer.Matthew@gmail.com>
2013-11-09 20:43:51 -0800

    Initial Commit

---
+++ Sheen/SheenAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SheenAppDelegate.h
+//  Sheen
+//
+//  Created by Matthew Ewer on 11/9/13.
+//  Copyright (c) 2013 CS193P - Matthew Ewer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SheenAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sheen/SheenViewController.h
@@ -0,0 +1,13 @@
+//
+//  SheenViewController.h
+//  Sheen
+//
+//  Created by Matthew Ewer on 11/9/13.
+//  Copyright (c) 2013 CS193P - Matthew Ewer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SheenViewController : UIViewController
+
+@end
