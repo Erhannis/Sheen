672f4e73e1ca156250044040f1d38f7fcaa63453
Matthew Ewer <Ewer.Matthew@gmail.com>
2013-11-19 03:20:23 -0800

    Messing around with colors and objects.

---
+++ Sheen/Debugging.h
@@ -0,0 +1,13 @@
+//
+//  Debugging.h
+//  Sheen
+//
+//  Created by Matthew Ewer on 11/19/13.
+//  Copyright (c) 2013 CS193P - Matthew Ewer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Debugging : NSObject
+
+@end
---
+++ Sheen/Debugging.m
@@ -0,0 +1,13 @@
+//
+//  Debugging.m
+//  Sheen
+//
+//  Created by Matthew Ewer on 11/19/13.
+//  Copyright (c) 2013 CS193P - Matthew Ewer. All rights reserved.
+//
+
+#import "Debugging.h"
+
+@implementation Debugging
+
+@end
